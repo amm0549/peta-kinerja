e64949fc9aa8ba4496a3b9031a435e49014d4841
amm0549 <104339171+amm0549@users.noreply.github.com>
2022-05-24 10:04:56 +0700

    separate scripts into their own js files

---
+++ js/map.js
@@ -0,0 +1,5 @@
+window.addEventListener("load", function() {
+        var svgObject = document.getElementById('svg-object').contentDocument;
+        var svg = svgObject.getElementById('svg5');
+        console.log(svg);
+    })
