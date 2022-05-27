c1bf02a53a3b2042af8daa8f73f19512b17b6913
amm0549 <104339171+amm0549@users.noreply.github.com>
2022-05-27 13:40:45 +0700

    add changes to layout

--- js/map.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener("load", function() {
-        var svgObject = document.getElementById('svg-object').contentDocument;
-        var svg = svgObject.getElementById('svg5');
-        console.log(svg);
-    })
